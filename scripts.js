678d871bbc20744ae0ef0da904ff4dad770461e9
raul <raulsilvab01@gmail.com>
2025-12-13 21:27:55 -0300

    Lógica JavaScript completa

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ let caixa = document.querySelector('.caixa')
 
 function abrirImagem(numero){
 
-    let url = `index.html?imagem=Imagem${numero}.jpg&descricao=${numero}`
+    let url = `imagem.html?imagem=Imagem${numero}.jpg&descricao=${numero}`
     window.open(url, '_blank')
 
 }
